1ef012055ab4e1d4b63e590623669e7d90c4e52f
Angelo <solonasusonly0@gmail.com>
2022-01-11 17:32:21 -0300

    Timer Button Up & Down

--- script.js
+++ script.js
@@ -52,6 +52,121 @@ const timerCount = (time, onoff) => {
     }
 };
 
+up.forEach(add => add.addEventListener('click', (e) => {
+    let num = 0;
+    switch (e.target.id) {
+        case 'up1':
+            
+            if(!(date[0].textContent >= '99')){
+                num = formField(++date[0].textContent);
+                date[0].textContent = num;
+                upd = (date[0].textContent * 86400);
+                total = upd + uph + upm + ups;
+            } else{
+                date[0].textContent = '00';
+                upd = (date[0].textContent * 86400);
+                total = upd + uph + upm + ups;
+            }
+        break;
+    
+        case 'up2':
+            if(!(date[1].textContent >= '23')){
+                num = formField(++date[1].textContent);
+                date[1].textContent = num;
+                uph = (date[1].textContent * 3600);  
+                total = upd + uph + upm + ups;
+            } else{
+                date[1].textContent = '00';
+                uph = (date[1].textContent * 3600);
+                total = upd + uph + upm + ups;
+            }
+        break;
+
+        case 'up3':
+            if(!(date[2].textContent >= '59')){
+                num = formField(++date[2].textContent);
+                date[2].textContent = num;
+                upm = (date[2].textContent * 60);
+                total = upd + uph + upm + ups;
+            } else{
+                date[2].textContent = '00';
+                upm = (date[2].textContent * 60);
+                total = upd + uph + upm + ups;
+            }
+        break;
+
+        default:
+            if(!(date[3].textContent >= '59')){
+                num = formField(++date[3].textContent);
+                date[3].textContent = num;
+                ups = parseInt(date[3].textContent);
+                total = upd + uph + upm + ups;
+            } else{
+                date[3].textContent = '00';
+                ups = 0;
+                total = upd + uph + upm + ups;
+            }
+        break;
+    }
+}));
+
+
+down.forEach(sub => sub.addEventListener('click', (e) => {
+    switch (e.target.id) {
+        case 'down1':
+            if (date[0].textContent <= '00') {
+                date[0].textContent = '99';
+                upd = (date[0].textContent * 86400);
+                total = upd + uph + upm + ups;
+            } else{
+                num = formField(--date[0].textContent);
+                date[0].textContent = num;
+                upd = (date[0].textContent * 86400);
+                total = upd + uph + upm + ups;
+            }
+        break;
+    
+        case 'down2':
+            if (date[1].textContent <= '00') {
+                date[1].textContent = '23';
+                uph = (date[1].textContent * 3600);
+                total = upd + uph + upm + ups;
+            } else{
+                num = formField(--date[1].textContent);
+                date[1].textContent = num;
+                uph = (date[1].textContent * 3600);
+                total = upd + uph + upm + ups;
+            }
+        break;
+
+        case 'down3':
+            if (date[2].textContent <= '00') {
+                date[2].textContent = '59';
+                upm = (date[2].textContent * 60);
+                total = upd + uph + upm + ups;
+            } else{
+                num = formField(--date[2].textContent);
+                date[2].textContent = num;
+                upm = (date[2].textContent * 60);
+                total = upd + uph + upm + ups;
+            }
+        break;
+
+        default:
+            if (date[3].textContent <= '00') {
+                date[3].textContent = '59';
+                ups = 0;
+                total = upd + uph + upm + ups;
+            } else{
+                num = formField(--date[3].textContent);
+                date[3].textContent = num;
+                ups = parseInt(date[3].textContent);
+                total = upd + uph + upm + ups;
+            }
+        break;
+    }
+}));
+
 start.onclick = function() {
     if (OF && total !== 0) {
         timerCount(total, true);
